af37200cbe5382ceb53d2d2f677d26c33b752a79
RasHerlo <rhj2111@columbia.edu>
2023-11-12 19:39:59 -0500

    Repaying the banter

--- StartUpConvo.py
+++ StartUpConvo.py
@@ -6,3 +6,5 @@ print('Yeah, I agree, think I just added a venv to the project?')
 # Ok, writing this in GitHub (RazzInCognito) and checking if it updates in PyCharm (RasHerlo)
 
 # It totally does, so IU will commit this back without asking for permission... (RasHerlo, PyCharm)
+
+# Writing this is the local PyCharm Branch, hoping to make pull request for main branch
