5a416621d0d7671004c1fd73180338103cfa6fef
RazzInCognito <150486844+RazzInCognito@users.noreply.github.com>
2023-11-12 19:26:35 -0500

    Update StartUpConvo.py
    Just continuing the banter

--- StartUpConvo.py
+++ StartUpConvo.py
@@ -2,3 +2,5 @@
 print("This is the start of something good")
 
 print('Yeah, I agree, think I just added a venv to the project?')
+
+# Ok, writing this in GitHub (RazzInCognito) and checking if it updates in PyCharm (RasHerlo)
